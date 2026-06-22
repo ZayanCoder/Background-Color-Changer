e1c8a2c85b75224785a2ae026f84bca5e56d51e3
ZayanCoder <khanzayanakbar@gmail.com>
2026-06-22 22:46:58 +0500

    Almost done

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-function changeColor(color){
-    document.body.style.backgroundColor = color;
+function changeColorRed(){
+    document.body.style.backgroundColor = "red";
+}
+
+function changeColorGreen(){
+    document.body.style.backgroundColor = "green";
+}
+
+function changeColorBlue(){
+    document.body.style.backgroundColor = "blue";
+}
+
+function changeColorOrange(){
+    document.body.style.backgroundColor = "orange";
 }
 
 function resetColor(){
